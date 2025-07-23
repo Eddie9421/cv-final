3b0ddf6aa9e46e6da5b309e6c9121477901eb341
Eddie <eddie11910@outlook.com>
2025-07-22 17:58:07 -0700

    Added some styling to the fieldset

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,12 @@ import Section from "./Section.jsx";
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center dark:bg-teal-950">
-      <h1 className="mt-16 font-medium text-shadow-lg dark:text-white ">
-        Bad CV Maker
-      </h1>
-      <Section></Section>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="mt-16 font-medium text-shadow-lg">Bad CV Maker</h1>
+      <Section sectionName="General Info">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" className="border-[1px]"></input>
+      </Section>
     </div>
   );
 }
