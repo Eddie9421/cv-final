0aa16ae4910747d312691c22d32fdb70961121e6
Eddie9421 <Eddie11910@outlook.com>
2025-08-06 23:51:52 -0700

    Made input field components hold some state to remember their values

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 export default function InputField({ id, labelText, type }) {
+  const [inputValue, setInputValue] = useState("");
+
+  function handleInputValueChange(e) {
+    setInputValue(e.target.value);
+  }
+
   return (
     <>
       <label htmlFor={id}>{labelText}</label>
-      <input type={type} id={id} className="border-[1px]"></input>
+      <input
+        type={type}
+        id={id}
+        onChange={handleInputValueChange}
+        value={inputValue}
+        className="border-[1px]"
+      ></input>
     </>
   );
 }
